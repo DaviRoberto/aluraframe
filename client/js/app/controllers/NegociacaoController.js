73effd34b5746ff0492fa07a6fb36537269cc01b
Davi Roberto <daviroberto11@hotmail.com>
2017-06-01 00:48:21 -0300

    Tratando a 'Data'

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,23 +1,27 @@
 // Obtendo valores para criar uma negociação de acordo com as regras de negócio da model 'Negociacao.js'
 class NegociacaoController{
-
+    
     constructor(){
         let $ = document.querySelector.bind(document);
-
+        
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');        
     }
-
+    
     adiciona(event){
         event.preventDefault();
-
-        // console.log(typeof(this._inputData.value));
-
-        let data = new Date(this._inputData.value.replace(/-/g, ','));
-       
+        // ... - spread operator ecmascript 6, desmembra o array da forma: 1º item do array é o 1º parametro do construtor do date;  2º item do array é o 2º parametro do construtor do date; e assim em diante. 
+        let data = new Date(
+        ...this._inputData.value
+            .split('-')
+            .map(function(item, indice) {
+                return item - indice % 2;
+            })
+        
+        );
         console.log(data);
-
+        
         // TODO: Adicionar negociação em uma lista
     }
 }
