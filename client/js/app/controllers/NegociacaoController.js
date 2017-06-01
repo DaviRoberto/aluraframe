138be665669474ae93da931a6da1d67b0083f6f5
Davi Roberto <daviroberto11@hotmail.com>
2017-06-01 01:04:52 -0300

    Comentários...

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -11,18 +11,19 @@ class NegociacaoController{
     
     adiciona(event){
         event.preventDefault();
-        // ... - spread operator ecmascript 6, desmembra o array da forma: 1º item do array é o 1º parametro do construtor do date;  2º item do array é o 2º parametro do construtor do date; e assim em diante. 
         let data = new Date(
-        ...this._inputData.value
-            .split('-')
-            .map((item, indice) => item - indice % 2)       
+            // ... - spread operator ecmascript 6, desmembra o array da forma: 1º item do array é o 1º parametro do construtor do date;  2º item do array é o 2º parametro do construtor do date; e assim em diante. 
+            ...this._inputData.value
+                .split('-')
+                .map((item, indice) => item - indice % 2)       
         );
         // Criando negociação
         let negociacao = new Negociacao(
             data,
             this._inputQuantidade.value,
             this._inputValor.value
-        )                
+        )   
+                     
         console.log(negociacao);
     }
 }
