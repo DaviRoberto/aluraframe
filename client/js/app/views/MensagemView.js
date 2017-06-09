252b8c2e7ece52c93179a8045b6c8bc0f4392a80
Davi Roberto <daviroberto11@hotmail.com>
2017-06-09 08:10:20 -0300

    View.js - Usando Herança

---
+++ client/js/app/views/MensagemView.js
@@ -0,0 +1,12 @@
+// extends - Herança
+class MensagemView extends View {
+
+    constructor(elemento){
+        super(elemento);
+    }
+
+    template(model){
+        return model.texto ? `<p class="alert alert-info">${model.texto}</p>` : '<p></p>';
+    }
+
+}
