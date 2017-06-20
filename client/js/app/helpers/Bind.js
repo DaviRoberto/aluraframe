0813f946e4baa1c73e5e5d992a51c6f53eb22adb
Davi Roberto <daviroberto11@hotmail.com>
2017-06-20 09:53:25 -0300

    Fazendo Bind

---
+++ client/js/app/helpers/Bind.js
@@ -0,0 +1,13 @@
+class Bind{
+
+    constructor(model, view, props){
+
+        let proxy = ProxyFactory.create(model, props, (mode) =>
+            view.update(model));
+        view.update(model);
+
+        return proxy;
+        
+    }
+
+}
