a01f2bad1c27cc58c13bd0c0b86a6458d3c03eca
Davi Roberto <daviroberto11@hotmail.com>
2017-06-23 12:23:55 -0300

    Usando IndexDB no projeto

---
+++ client/js/app/services/ConnectionFactory.js
@@ -0,0 +1,50 @@
+var stores = ['negociacoes'];
+var version = 10;
+var dbName = 'frameNegociacao';
+
+var connection = null;
+
+class ConnectionFactory{
+
+    constructor() {
+        throw new Error('Não é possível criar instância de ConnectionFactory.');
+    }
+
+    static getConnection(){
+        return new Promise((resolve, reject) => {
+
+            let openRequest = window.indexedDB.open(dbname, version);
+
+            openRequest.onupgradeneeded = e => {
+                
+              ConnectionFactory._createStores(e.target.result);
+
+            };
+
+            openRequest.onsuccess = e => {
+                if(!connection) connection = e.target.result;
+                resolve(connection);
+
+            };
+
+            openRequest.onerror = e => {
+
+                console.log(e.target.error);
+
+                reject(e.target.error.name);
+            };
+
+        });
+    }
+
+    static _createStores(connection) {
+
+        stores.forEach(store => {
+            
+                if (connection.objectStoreNames.contains(store)) 
+                    connection.deleteObjectStore(store);
+                connection.createObjectStore(store, { autoIncrement: true });
+        });
+    }
+
+}
