b54d09d180d430ac65a1edc7fa1a45853ebfb55c
Davi Roberto <daviroberto11@hotmail.com>
2017-06-27 15:07:14 -0300

    Melhorando requisições Ajax com a Fetch API - ES-2016

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,30 +1,25 @@
 class HttpService{
 
+    _handleErros(res){
+        if(!res.ok) throw new Error(res.statusText);
+        return res;
+    }
+
     get (url) {
 
-        return new Promise((resolve, reject) => {
+        return fetch(url)
+            .then(res => this._handleErros(res))
+            .then(res => res.json());
+    }
 
-            let xhr = new XMLHttpRequest();
+    post(url, dado) {
 
-            xhr.open('GET', url);
-
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
-
-                        resolve(JSON.parse(xhr.responseText));
-                        
-                    } else {
-                        console.log(xhr.responseText);
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-
-            xhr.send();
-
-        });
-
+        return fetch(url, {
+            headers: { 'Content-Type': 'application/json' },
+            method: 'post',
+            body: JSON.stringify(dado)
+        })
+        .then(res => this._handleErrors(res));
     }
 
 }
